d7a741d04e2440a8af49b6528bfbb4c5979303d5
SimonGebru <te.gebru@hotmail.se>
2025-08-31 17:03:59 +0200

    test

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/', 
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'), 
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+  },
 })
