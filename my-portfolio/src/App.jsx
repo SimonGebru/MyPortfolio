11cfd6d58b0ea1552bee98d8333a077c7e92bcc7
SimonGebru <te.gebru@hotmail.se>
2025-08-31 16:59:26 +0200

    sista fixen, flyttat min browserrouter

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProjectPage from './pages/ProjectPage';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectPage />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <--- Lägg till!
+import './index.css';
+import App from './App.jsx';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -9,6 +10,8 @@ AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
