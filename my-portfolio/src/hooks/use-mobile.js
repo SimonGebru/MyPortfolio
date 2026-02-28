f1fb41bfb449bc51648557e22bd320a6f32240c4
SimonGebru <te.gebru@hotmail.se>
2026-02-28 10:47:54 +0100

    mobilvy

---
+++ my-portfolio/src/hooks/use-mobile.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState(undefined);
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    // Kör direkt så värdet finns från start
+    onChange();
+
+    // Safari fallback
+    if (mql.addEventListener) mql.addEventListener("change", onChange);
+    else mql.addListener(onChange);
+
+    return () => {
+      if (mql.removeEventListener) mql.removeEventListener("change", onChange);
+      else mql.removeListener(onChange);
+    };
+  }, []);
+
+  return !!isMobile;
+}
